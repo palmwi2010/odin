b42b8422a39a289893c451ac3afab4d879ccf8bd
palmwi2010 <wop3000@hotmail.co.uk>
2024-07-24 11:59:06 +0100

    Added ui

--- rock-paper-scissors/scripts.js
+++ rock-paper-scissors/scripts.js
@@ -10,15 +10,6 @@ let getComputerChoice = () => {
     return options[choice]
 }
 
-let getHumanChoice = () => {
-    let choice = ''
-
-    while (!options.includes(choice)) {
-        choice = prompt("Please enter 'rock', 'paper' or 'scissors'.").toLowerCase()
-    }
-    return choice
-}
-
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
@@ -26,24 +17,34 @@ function capitalizeFirstLetter(string) {
 let winMessage = (humanChoice, computerChoice, result) => {
     humanChoice = capitalizeFirstLetter(humanChoice);
     computerChoice = capitalizeFirstLetter(computerChoice);
+
+    const resultDiv = document.querySelector('.result-div');
+    resultDiv.style.display = 'block';
+
+    const humanScoreBoard = document.querySelectorAll('.score')[0];
+    const computerScoreBoard = document.querySelectorAll('.score')[1]; 
+    const resultMessage = document.querySelector('.result-msg');
+
     switch (result) {
         case 'tie':
-            console.log(`Tie! Both players picked ${humanChoice}.`);
+            resultMessage.innerText =`Tie! Both players picked ${humanChoice}.`;
             break;
         case 'human':
             humanScore++;
-            console.log(`Human wins! ${humanChoice} beats ${computerChoice}.`);
+            humanScoreBoard.innerText = humanScore;
+            resultMessage.innerText = `Human wins! ${humanChoice} beats ${computerChoice}.`;
             break;
         case 'computer':
             computerScore++;
-            console.log(`Computer wins! ${computerChoice} beats ${humanChoice}.`);
+            computerScoreBoard.innerText = computerScore;
+            resultMessage.innerText = `Computer wins! ${computerChoice} beats ${humanChoice}.`;
             break;
     }   
 }
 
 let playRound = (humanChoice, computerChoice) => {
 
-    // Get indices chosen and calculat gape
+    // Get indices chosen and calculate gape
     let humanIndex = optionToIndex[humanChoice];
     let computerIndex = optionToIndex[computerChoice];
     let delta = humanIndex - computerIndex;
@@ -59,22 +60,71 @@ let playRound = (humanChoice, computerChoice) => {
         winMessage(humanChoice, computerChoice, 'computer');
     }
     else {
-        winMessage(humanChoice, computerChoice, 'tie');
+        return winMessage(humanChoice, computerChoice, 'tie');
     }
 }
 
-let playGame = rounds =>  {
-    for (let i = 0; i < rounds; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
+let setup = () => {
+    const body = document.querySelector('body');
+    
+    const header = document.createElement('h1');
+    header.innerText = 'Rock paper scissors';
+    body.appendChild(header);
+
+    const subHeader = document.createElement('h2');
+    subHeader.innerText = 'Human: Please select your object.'
+    body.appendChild(subHeader);
+
+    const buttonDiv = document.createElement('div');
+    buttonDiv.style = "display: flex; justify-content: space-between;"
+
+    for (let i = 0; i < options.length; i++) {
+        let btn = document.createElement('button');
+        btn.innerText = capitalizeFirstLetter(options[i]);
+        btn.addEventListener('click', () => {
+            playRound(btn.innerText.toLowerCase(), getComputerChoice());
+        })
+        buttonDiv.appendChild(btn);
+    }
+    body.appendChild(buttonDiv);
+
+    const resultDiv = document.createElement('div');
+    resultDiv.classList.add('result-div')
+
+    const resultHeader = document.createElement('h2');
+    resultHeader.innerText = "Results";
+
+    const scoreBoard = document.createElement('div');
+    scoreBoard.classList.add('scoreboard');
+    
+    // Scoreboard
+    for (let i = 0; i < 2; i++) {
+        let scoreCard = document.createElement('div');
+        scoreCard.classList.add('card');
+        let playerName = document.createElement('span');
+        playerName.classList.add('player-name');
+        
+        let playerScore = document.createElement('span');
+        playerScore.classList.add('score');
+
+        playerName.innerText = (i === 0) ? 'Human': 'Computer';
+        playerScore.innerText = (i===0) ? humanScore: computerScore;
+
+        scoreCard.appendChild(playerName);
+        scoreCard.appendChild(playerScore);
+        scoreBoard.appendChild(scoreCard);
     }
 
-    if (computerScore > humanScore) {
-        console.log(`Computer wins! Final scores: Human ${humanScore}, Computer ${computerScore}`);
-    } else if (humanScore > computerScore) {
-        console.log(`Human wins! Final scores: Human ${humanScore}, Computer ${computerScore}`);
-    } else {
-        console.log(`Tie! Final scores: Human ${humanScore}, Computer ${computerScore}`);
-    }
+    resultDiv.appendChild(resultHeader);
+    resultDiv.appendChild(scoreBoard);
+
+    // Game result message
+    const resultMessage = document.createElement('p');
+    resultMessage.classList.add('result-msg');
+
+    resultDiv.appendChild(resultMessage);
+
+    body.appendChild(resultDiv);
 }
 
-playGame(5);
+setup();
